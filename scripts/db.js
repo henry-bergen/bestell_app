6d8391ba0d9f3ada7f4ac8a5f2a723130bba80ae
Henry Bergen <henry.bergen@gmx.de>
2025-10-30 15:31:19 +0100

    create: basic website structure

---
+++ scripts/db.js
@@ -0,0 +1,64 @@
+let dishes = [
+  {
+    name: "borsch",
+    price: "12,50",
+    description:
+      "Traditioneller Rote-Bete-Eintopf aus Rinderbrühe und Gemüse, dazu Sauerteigbrot und Schmand",
+  },
+  {
+    name: "Ähzezupp",
+    price: "9,50",
+    description:
+      "Rheinische Erbsensuppe mit Würstchen, Speck und Gemüse – kräftig und herzhaft",
+  },
+  {
+    name: "Plootdietsche Bratkartoffeln",
+    price: "11,20",
+    description: "Mit Speck, Zwiebeln & Spiegelei",
+  },
+  {
+    name: "Kappes und Speck",
+    price: "13,90",
+    description:
+      "Deftig geschmorter Weißkohl mit Speckwürfeln und Kartoffeln – wie bei Oma im Rheinland",
+  },
+  {
+    name: "Pelmeni",
+    price: "14,80",
+    description: "Gefüllte Teigtaschen mit Hackfleisch & Butterzwiebeln",
+  },
+  {
+    name: "Rheinischer Sauerbraten mit Kartoffelklößen und Soß",
+    price: "16,50",
+    description: "Braten, Klöße, dazu Rosinensauce und Rotkohl",
+  },
+  {
+    name: "Schaschlik",
+    price: "15,90",
+    description:
+      "Marinierte Fleischspieße vom Grill mit Zwiebeln, Tomaten und hausgemachter Sauce",
+  },
+  {
+    name: "Galubzij",
+    price: "13,50",
+    description:
+      "Kohlrouladen mit Hackfleisch und Reisfüllung, in kräftiger Tomatensauce",
+  },
+  {
+    name: "Plov",
+    price: "14,20",
+    description:
+      "Asiatisches Reisgericht nach kasachischer Art, mit Lamm, Karotten und Zwiebeln ",
+  },
+  {
+    name: "Kölsche Rievkooche",
+    price: "10,90",
+    description: "Knusprige Reibekuchen mit Apfelmus",
+  },
+  {
+    name: "Blini mit Sguschonka",
+    price: "8,90",
+    description:
+      "Russische Pfannkuchen, warm serviert mit karamelisierter Kondensmilch",
+  },
+];
