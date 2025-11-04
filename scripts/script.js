e08fc04a47f5edfe9dd9f1b68836a0411ad0f2b4
Henry Bergen <henry.bergen@gmx.de>
2025-11-04 04:53:55 +0100

    finish: everything for handing in project

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,84 @@
+const basket = [];
+
 function init() {
+  getFromLocalStorage();
   renderDishes();
+  renderBasket();
 }
 
 function renderDishes() {
-  const dishContainer = document.getElementById("dishes");
-  dishContainer.innerHTML = "";
+  const menuDishesContainer = document.getElementById("dishes");
+  menuDishesContainer.innerHTML = "";
   for (let i = 0; i < dishes.length; i++) {
-    dishContainer.innerHTML += getDishCardHtml(dishes[i]);
+    menuDishesContainer.innerHTML += getDishCardHtml(dishes[i], i);
   }
 }
 
-function addDishToBasket() {
-  document.getElementById("dishes").innerHTML;
+function addDishToBasket(i) {
+  const dish = dishes[i];
+  const basketItem = basket.find((element) => element.name === dish.name);
+
+  if (basketItem) {
+    basketItem.amount += 1;
+  } else {
+    basket.push({
+      name: dish.name,
+      amount: 1,
+      basePrice: parseFloat(dish.price.replace(",", ".")),
+    });
+  }
+
+  renderBasket();
+}
+
+function saveToLocalStorage() {
+  localStorage.setItem("basket", JSON.stringify(basket));
+}
+
+function getFromLocalStorage() {
+  let myArr = JSON.parse(localStorage.getItem("basket"));
+  if (myArr) {
+    basket.length = 0;
+    basket.push(...myArr);
+  }
+}
+
+function renderBasket() {
+  const basketDishesContainer = document.getElementById("basketItems");
+  basketDishesContainer.innerHTML = "";
+
+  for (let i = 0; i < basket.length; i++) {
+    basketDishesContainer.innerHTML += getBasketDishHtml(basket[i], i);
+  }
+
+  const basketSummaryContainer = document.getElementById("basketSummary");
+  const { subtotal, shipping, total } = calculateTotals();
+
+  renderSummary(basketSummaryContainer, subtotal, shipping, total);
+  saveToLocalStorage();
+}
+
+function calculateTotals() {
+  let subtotal = 0;
+  for (let i = 0; i < basket.length; i++) {
+    subtotal += basket[i].basePrice * basket[i].amount;
+  }
+  const shipping = 5;
+  const total = subtotal + shipping;
+  return { subtotal, shipping, total };
+}
+
+function renderSummary(container, subtotal, shipping, total) {
+  container.innerHTML = getBasketSummaryHtml(subtotal, shipping, total);
+}
+
+function increaseAmount(i) {
+  basket[i].amount += 1;
+  renderBasket();
+}
+
+function decreaseAmount(i) {
+  basket[i].amount -= 1;
+  if (basket[i].amount < 1) basket.splice(i, 1);
+  renderBasket();
 }
--- scripts/template.js
+++ scripts/template.js
@@ -1,25 +1,50 @@
-function getDishCardHtml(dish) {
+function getDishCardHtml(dish, i) {
   return `        
     <div class="dishCard">
-        <div class="dishInfo">
-            <h3>${dish.name}</h3>
-            <p class="dishDescription">${dish.description}</p>
-            <p class="dishPrice">${dish.price} €</p>
-        </div>
-        <button onclick="addDishToBasket(dish)" class="addBtn">+</button>
+      <div class="dishInfo">
+        <h3>${dish.name}</h3>
+        <p class="dishDescription">${dish.description}</p>
+        <p class="dishPrice">${dish.price} €</p>
+      </div>
+      <button onclick="addDishToBasket(${i})" class="addBtn">+</button>
     </div>
     `;
 }
 
-function getBasketHTML(dish) {
-  return `     
-    <span>${dish.name}</span>
-    <div class="qtyControl">
-        <button>-</button>
-        <span>3x</span>
-        <button>+</button>
+function getBasketDishHtml(item, i) {
+  return `
+    <div class="basketItem">
+      <span>${item.name}</span>
+      <div class="basketItemNumbers">
+        <div class="qtyControl">
+          <button onclick="decreaseAmount(${i})">-</button>
+          <span>${item.amount}x</span>
+          <button onclick="increaseAmount(${i})">+</button>
+        </div>
+        <span class="dishPrice"
+          >${(item.basePrice * item.amount)
+            .toFixed(2)
+            .replace(".", ",")} €</span
+        >
+      </div>
     </div>
-    <span class="dishPrice">${dish.price} €</span>
     <hr />
     `;
+}
+
+function getBasketSummaryHtml(subtotal, shipping, total) {
+  return `
+    <div class="basketSummaryItem">
+      <span>Zwischensumme</span>
+      <span class="dishPrice">${subtotal.toFixed(2).replace(".", ",")} €</span>
+    </div>
+    <div class="basketSummaryItem">
+      <span>Lieferkosten</span>
+      <span class="dishPrice">${shipping.toFixed(2).replace(".", ",")} €</span>
+    </div>
+    <div class="basketSummaryItem">
+      <strong>Gesamt</strong>
+      <strong class="dishPrice">${total.toFixed(2).replace(".", ",")} €</strong>
+    </div>
+  `;
 }
